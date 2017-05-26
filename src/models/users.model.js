78776a660c42fdd03381658c055ae13f1351f7b4
Harvey Sanders <sanders.harvey@gmail.com>
2017-05-26 12:33:36 -0500

    Initial commit

---
+++ src/models/users.model.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const users = sequelizeClient.define('users', {
+  
+  
+  }, {
+    classMethods: {
+      associate (models) { // eslint-disable-line no-unused-vars
+        // Define associations here
+        // See http://docs.sequelizejs.com/en/latest/docs/associations/
+      }
+    }
+  });
+
+  return users;
+};
